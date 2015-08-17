3e82b01170b99fe04eeae3c6934029573b897a1c
Yusef Napora <yusef@napora.org>
2015-08-16 21:26:44 -0400

    openstruct for starwars data

--- spec/support/star_wars_data.rb
+++ spec/support/star_wars_data.rb
@@ -1,31 +1,31 @@
+require 'ostruct'
 
+xwing = OpenStruct.new({id: '1', name: 'X-Wing'})
+ywing = OpenStruct.new({id: '2', name: 'Y-Wing'})
+awing = OpenStruct.new({id: '3', name: 'A-Wing'})
+falcon = OpenStruct.new({id: '4', name: 'Millenium Falcon'})
+home_one = OpenStruct.new({id: '5', name: 'Home One'})
 
-xwing = {id: '1', name: 'X-Wing'}
-ywing = {id: '2', name: 'Y-Wing'}
-awing = {id: '3', name: 'A-Wing'}
-falcon = {id: '4', name: 'Millenium Falcon'}
-home_one = {id: '5', name: 'Home One'}
+tie_fighter = OpenStruct.new({id: '6', name: 'TIE Fighter'})
+tie_interceptor = OpenStruct.new({id: '7', name: 'TIE Interceptor'})
+executor = OpenStruct.new({id: '8', name: 'Executor'})
 
-tie_fighter = {id: '6', name: 'TIE Fighter'}
-tie_interceptor = {id: '7', name: 'TIE Interceptor'}
-executor = {id: '8', name: 'Executor'}
-
-rebels = {
+REBELS = OpenStruct.new({
     id: '1',
     name: 'Alliance to Restore the Republic',
     ships: %w{1 2 3 4 5}
-}
+})
 
-empire = {
+EMPIRE = OpenStruct.new({
     id: '2',
     name: 'Galactic Empire',
     ships: %w{6 7 8}
-}
+})
 
-data = {
+DATA = {
     Faction: {
-        '1' => rebels,
-        '2' => empire
+        '1' => REBELS,
+        '2' => EMPIRE
     },
     Ship: {
         '1' => xwing,
@@ -39,30 +39,32 @@ data = {
     }
 }
 
-next_ship = 9
+$next_ship = 9
 
 def create_ship(ship_name, faction_id)
+  $next_ship += 1
+  ship_id = $next_ship
   new_ship = {
-      id: (next_ship++).to_s,
+      id: ship_id.to_s,
       name: ship_name
   }
-  data[:Ship][new_ship[:id]] = new_ship
-  data[:Faction][faction_id].ships << new_ship.id
+  DATA[:Ship][new_ship[:id]] = new_ship
+  DATA[:Faction][faction_id].ships << new_ship.id
   new_ship
 end
 
 def get_ship(id)
-  data[:Ship][id]
+  DATA[:Ship][id]
 end
 
 def get_faction(id)
-  data[:Faction][id]
+  DATA[:Faction][id]
 end
 
 def get_rebels
-  rebels
+  REBELS
 end
 
 def get_empire
-  empire
+  EMPIRE
 end
